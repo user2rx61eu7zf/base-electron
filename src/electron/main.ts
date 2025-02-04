c7340a09700a16fec8e56ae7ecbb6321a912743e
user2rx61eu7zf <negantwd68@gmail.com>
2025-02-04 14:09:10 +0100

    Initial commit

---
+++ src/electron/main.ts
@@ -0,0 +1,15 @@
+import {app,BrowserWindow} from 'electron';
+import path from 'path';
+import {isDev} from './util.js'
+
+type test =string;
+
+app.on('ready',()=>{
+    const mainWindow=new BrowserWindow({})
+    if (isDev()){
+            mainWindow.loadURL('http://localhost:8080/')
+    }else{
+        mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html'));
+    }
+   
+});
